842a222c1ed1fbe25bf889728c145bb3c434ff60
Bostan <bostankhan6@gmail.com>
2021-08-25 09:16:46 +0500

    commit

--- src/main.py
+++ src/main.py
@@ -7,15 +7,13 @@ import loss
 from option import args
 from trainer import Trainer
 
-from prettytable import PrettyTable
 
 def count_parameters(model):
-    table = PrettyTable(["Modules", "Parameters"])
+
     total_params = 0
     for name, parameter in model.named_parameters():
         if not parameter.requires_grad: continue
         param = parameter.numel()
-        table.add_row([name, param])
         total_params+=param
     #print(table)
     print(f"Total Trainable Params: {total_params}")
